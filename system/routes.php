8dee18eda5dfaf3718308a77d13d1ff26d5fc96e
Anthony Persson <15910972+joelslamospersson@users.noreply.github.com>
2025-11-07 18:12:37 +0100

    Create routes.php
    Adding re-routing option

---
+++ system/routes.php
@@ -0,0 +1,9 @@
+[['GET', 'POST'], 'monsters/{name:string}', 'monsters.php'],
+
+/**
+ * Added for easy re-routing over menu page
+ * Add this between the above and below line, or elsewhere
+ */
+[['GET', 'POST'], 'coins', 'coins.php'],
+
+[['GET', 'POST'], 'faq/{action:string}', 'faq.php'],
